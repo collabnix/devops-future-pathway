6cdf27a4cc4a98df39639d25ccb9baef168021ff
Collabnix <34368930+collabnix@users.noreply.github.com>
2025-06-29 09:48:40 +0530

    Add routes for new pages: Resources, Tools, Careers, Certifications, Community, Blog

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Resources from "./pages/Resources";
+import Tools from "./pages/Tools";
+import Careers from "./pages/Careers";
+import Certifications from "./pages/Certifications";
+import Community from "./pages/Community";
+import Blog from "./pages/Blog";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -19,6 +25,12 @@ const App = () => (
       <BrowserRouter basename={basename}>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/tools" element={<Tools />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/certifications" element={<Certifications />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/blog" element={<Blog />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
